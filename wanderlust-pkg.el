a642423f7fee4a55406501ea896e17c7cf9e3133
Jonas Bernoulli <jonas@bernoul.li>
2017-08-19 17:51:00 +0200

    [PATCH] Add library wanderlust.el whose name matches that of the package wanderlust
    A package should always contain a library with the same name.  One
    benefit of that is that Melpa can then extract the package description
    from library commentary.
    
    Remove "wanderlust-pkg.el".  The only additional information it contains
    is a version string, which has not ever been bumped since it was first
    added in 2014.
    
    [Emacsmirror] This commit is being rebased onto upstream.

--- wanderlust-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "wanderlust" "2.15.9"
-  "Yet Another Message Interface on Emacsen"
-  '((emacs "24.5")
-    (apel "10.8")
-    (flim "1.14.9")
-    (semi "1.14.7"))
-  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
